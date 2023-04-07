a6295d08ab37915da8c9025643fa04776b5eb479
Anand Kushwaha <98548977+ANandasaad@users.noreply.github.com>
2023-04-07 17:06:26 +0530

    readandwrite

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const fs= require("fs");
+//   fs.writeFileSync('read.txt',"Hello my name is Anand Kushwaha");
+//  const buf= fs.readFileSync('read.txt');
+//  console.log(buf.toString());
+
+ fs.appendFileSync("read.txt","\nI am Loving Node js");
+ fs.renameSync('read.txt',"readwrite.txt");
+
+
+
+ const posix = require('posix');
+
+const systemInfo = posix.uname();
+
+console.log(systemInfo);
